f6c72ca77b41fff9ce18c935c5301dedef281349
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-11-26 09:48:21 -0500

    aggregator can produce multidimensional representation of past hits

--- lib/training_aggregator.rb
+++ lib/training_aggregator.rb
@@ -1,7 +1,22 @@
 require 'file_handler'
+require 'game_state'
 module TrainingAggregator
 
   def self.trained?
     FileHandler.get_all.count > 1
   end
+
+  def self.aggregate
+    model = Array.new(100, 0)
+    FileHandler.get_all.each do |filename|
+      complete_state = GameState.load(filename).flatten
+      complete_state.each_with_index do |state, index|
+        if (state == :hit)
+          model[index] += 1
+        end
+      end
+    end
+    model.each_slice(10).to_a
+  end
+
 end
--- spec/training_aggregator_spec.rb
+++ spec/training_aggregator_spec.rb
@@ -1,6 +1,20 @@
 require 'training_aggregator'
+require 'board_saver'
 
 describe 'training aggregator' do
+
+  let(:state){
+    state = []
+    10.times do |row|
+      column = []
+      10.times do |thing|
+        column << :unknown
+      end
+      state << column
+    end
+    state
+  }
+
   SNAPSHOTS_DIR = 'snapshots'
 
   after(:each) do
@@ -17,5 +31,27 @@ describe 'training aggregator' do
     expect(TrainingAggregator.trained?).to eq false
     File.open("#{SNAPSHOTS_DIR}/2.yml", 'w') {|f| f.write "" }
     expect(TrainingAggregator.trained?).to eq true
+  end
+
+  it 'can produce an aggregate of all training expressed as a count of hits for each coordinate' do
+    board_saver = BoardSaver.new
+    state[0][0] = :hit
+    board_saver.update(state)
+    state[0][1] = :hit
+    new_board_saver = BoardSaver.new
+    new_board_saver.update(state)
+    found = TrainingAggregator.aggregate
+    expected = []
+    10.times do |row|
+      column = []
+      10.times do |thing|
+        column << 0
+      end
+      expected << column
+    end
+    expected[0][0] = 2
+    expected[0][1] = 1
+    expect(found).to eq expected
+
   end
 end
